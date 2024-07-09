0135937b634f1b5f7b61e10319d27663f1c9bf81
Sebastien Auriault <sebastienauriault@gmail.com>
2024-07-09 18:05:58 +0900

    Fix tests not having credentials

--- config/initializers/openai.rb
+++ config/initializers/openai.rb
@@ -1,5 +1,5 @@
 OpenAI.configure do |config|
-  config.access_token = Rails.application.credentials.open_ai[:api_key]
+  config.access_token = Rails.application.credentials.dig(:open_ai, :api_key)
   # config.organization_id = ENV.fetch("OPENAI_ORGANIZATION_ID") # Optional
   config.log_errors = true # Highly recommended in development, so you can see what errors OpenAI is returning. Not recommended in production because it could leak private data to your logs.
 end
